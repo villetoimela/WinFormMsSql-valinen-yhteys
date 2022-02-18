f179fd9b85a3acc1f05b742db552264cac823a2e
Ville Toimela <ville.toimela@hotmail.com>
2022-02-18 11:31:28 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WinFormMsSql_valinen_yhteys
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection myConnection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Opiskelijat;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void SqlConnectionAvattu()
+        {
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void SqlConnetionSuljettu()
+        {
+            try
+            {
+                myConnection.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+
+
+    }
+}
